3c61aaffb9aec13da33b2920cee5ba0125a869a4
ziyu <ziyu@caibaopay.com>
2018-01-18 17:01:22 +0800

    ..

--- bin/new-branch.js
+++ bin/new-branch.js
@@ -1,58 +1,5 @@
 const inquirer = require('inquirer');
 const shell = require('shelljs');
-// const {gitlabUrl,gitlabToken} = require('../config.json');
-// const gitlab = require('gitlab')({
-//     url: gitlabUrl,
-//     token: gitlabToken
-// });
-// function getGroups() {
-//     return new Promise((resolve)=>{
-//         gitlab.groups.all(function(groups) {
-//             let groupList = groups.map(item=>{
-//                 return {name:item.name,value:item.id,};
-//             });
-//             resolve(groupList)
-//         });
-//     });
-// }
-
-// function getProjectsByGroupId(id) {
-//     return new Promise((resolve)=>{
-//        gitlab.groups.listProjects(id,(projects)=>{
-//            console.log(projects);
-//
-//            let projectList = projects.map(item=>{
-//                return {name:item.name,value:item.id};
-//            });
-//            resolve(projectList);
-//        });
-//     });
-// }
-
-// async function createHotfix() {
-//     let {branchName} = await inquirer.prompt({
-//         type: 'list',
-//         name: 'groupId',
-//         message: '请选择项目所在的group',
-//         choices: await getGroups()
-//     });
-//     let {projectId} = await inquirer.prompt({
-//         type:'list',
-//         name:'projectId',
-//         message:'请选择项目',
-//         choices:await getProjectsByGroupId(groupId)
-//     });
-//     console.log(projectId);
-//     let {branch} = await inquirer.prompt({
-//         type: 'input',
-//         name: 'branch',
-//         message: "请输入分支名"
-//     });
-//     gitlab.projects.init();
-//     gitlab.projects.repository.createBranch({projectId,branch:branch||"nima",ref:'master'},(a,b,c)=>{
-//         console.log(a,b,c);
-//     })
-// }
 
 async function createBranch() {
     let {commitMsg} = await inquirer.prompt({
