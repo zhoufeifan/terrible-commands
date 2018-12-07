ff828c5181730b177ae9ddfa3bea450b97de3ffc
zhoufeifan <xieyang.zff@raycloud.com>
2018-12-07 16:44:20 +0800

    更新发布脚本

--- nima.js
+++ nima.js
@@ -3,10 +3,27 @@ const puppeteer = require('puppeteer-core');
   const browser = await puppeteer.launch({
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     headless: false,
+    slowMo: 250,
     userDataDir: '/Users/xieyang/Library/Application Support/Google/Chrome/Profile 1'
   });
   const page = await browser.newPage();
+  page.setViewport({
+    width: 1440,
+    height: 900
+  })
   await page.goto('https://sync.superboss.cc/index.xhtml#/html/publish');
-  await page.evaluate(() => alert('nima'));
-  // await browser.close();
+  // await page.goto('https://www.baidu.com');
+  // await page.evaluate(() => alert('nima'));
+  const hasLogin = await page.$eval('title', el => {
+    const title = el.text
+    return title && title !== "二维码登录"
+  });
+  if(!hasLogin){
+    console.log('请先登录')
+    await page.evaluate(() => alert('未登录'));
+    await browser.close();
+  }
+  await page.evaluate(() => {
+    // const
+  });
 })();
